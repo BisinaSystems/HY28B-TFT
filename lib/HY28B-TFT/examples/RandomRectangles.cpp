896fd03ce75d256264c147bc1a5327bc674e48ba
David Perish <david@stonyhillsoftware.com>
2024-01-12 18:51:56 -0500

    Added colors, line and rect w/new example

--- lib/HY28B-TFT/examples/RandomRectangles.cpp
+++ lib/HY28B-TFT/examples/RandomRectangles.cpp
@@ -1,3 +1,7 @@
+/**
+ * Draws random colored rectangles on an HY28B-TFT display
+ */
+
 #include <Arduino.h>
 #include <SPI.h>
 
@@ -22,7 +26,7 @@ void setup() {
   spi = new SPIClass(VSPI);
   spi->begin(SCLK, MISO, MOSI, CS);
 
-  display.init(spi, CS, RESET);
+  display.init(spi, CS, RESET, Orientation::HORIZONTAL, 78000000U);
   display.clear(Color::royalblue);
 }
 
@@ -35,5 +39,8 @@ void loop() {
   auto y2 = (unsigned short) random(0, 239);
   auto c = (unsigned short) random(0, 65535);
 
-  display.drawRectangle(x1, y1, x2, y2, c);
+  display.drawLine(x1, y1, x2, y1, c);
+  display.drawLine(x2, y1, x2, y2, c);
+  display.drawLine(x2, y2, x1, y2, c);
+  display.drawLine(x1, y2, x1, y1, c);
 }
